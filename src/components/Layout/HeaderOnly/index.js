b7d5ae50c9270ff23ab7632608e41684ff4e80b5
Vu Van Hieu <HieuVV@ltsgroup.tech>
2024-03-18 18:13:53 +0700

    Bài 63

--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,5 +1,11 @@
-function HeaderOnly() {
-    return <h1>HeaderOnly</h1>;
+import Header from './components/Layout/components/Header';
+
+function HeaderOnly({ children }) {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
 }
 
 export default HeaderOnly;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import { HeaderOnly } from '~/components/Layout';
-
+import cx from 'classnames/bind';
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
+import classNames from 'classnames';
+
+const res = cx(classNames.bind);
 
 const publicRoutes = [
     { path: '/', component: Home },
