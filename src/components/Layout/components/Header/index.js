6216dc0d634a36251446349f371cf11cc2b3c86c
Vu Van Hieu <HieuVV@ltsgroup.tech>
2024-03-19 11:37:17 +0700

    logo

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,17 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/index';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok"></img>
+                </div>
+            </div>
         </header>
     );
 }
